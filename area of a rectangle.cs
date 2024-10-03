45efe1a8254ac7b2cd8ad6dfe6e6895619d96a51
ryhann098 <40297224@hruc.ac.uk>
2024-10-03 15:06:41 +0100

    Add files via upload

---
+++ area of a rectangle.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+{
+
+    double length;
+    double width;
+    //
+    Console.WriteLine("enter the length of the rectangle");
+    length = Convert.ToDouble(Console.ReadLine());
+    //
+    Console.WriteLine("enter width of rectangle");
+    width = Convert.ToDouble(Console.ReadLine());
+    //
+    double area = calculatingArea(length, width);
+    //
+    Console.WriteLine($"the earea of a area is {area}");
+}
+//
+static double calculatingArea(double length, double width)
+{
+    return length * width;
+}
+
